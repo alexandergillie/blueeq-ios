6ef21e58ec302fbda548d75d3b230e7378a8acef
Alex Gillie <alexander.gillie@insidesales.com>
2019-01-31 21:38:44 -0700

    Initial Commit

---
+++ BlueEQ Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueEQ Prototype
+//
+//  Created by Alexander Gillie on 1/31/19.
+//  Copyright © 2019 Alexander Gillie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
