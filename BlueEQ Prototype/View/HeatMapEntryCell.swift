34f5e621df34fc29dba3dad2cb33dfafd24ae7b0
Alex Gillie <alexander.gillie@insidesales.com>
2019-02-25 19:40:29 -0700

    adding color and score to heat map

---
+++ BlueEQ Prototype/View/HeatMapEntryCell.swift
@@ -0,0 +1,16 @@
+//
+//  HeatMapEntryCell.swift
+//  BlueEQ Prototype
+//
+//  Created by Alexander Gillie on 2/25/19.
+//  Copyright © 2019 Alexander Gillie. All rights reserved.
+//
+
+import UIKit
+
+class HeatMapEntryCell: UITableViewCell {
+    
+    @IBOutlet weak var dimensionName: UILabel!
+    
+    @IBOutlet weak var dimensionScore: UILabel!
+}
